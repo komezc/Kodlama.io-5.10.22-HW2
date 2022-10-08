b3b5fde443ed74dc5da1d76534b11633fa63ef03
komezc <88783106+komezc@users.noreply.github.com>
2022-10-08 20:15:10 +0300

    Kodlama.io 5.10.22 HW2

---
+++ interfaces/src/MySqlCustomerDal.java
@@ -0,0 +1,7 @@
+public class MySqlCustomerDal implements ICustomerDal, IRepositpry{
+
+    @Override
+    public void add() {
+        System.out.println("My sql eklendi");
+    }
+}
---
+++ staticDemo/src/ProductValidator.java
@@ -0,0 +1,33 @@
+public class ProductValidator {
+    static{
+        System.out.println("Static blok calisti");
+    }
+    static{
+        System.out.println("Static blok calisti");
+    }
+    static{
+        System.out.println("Static blok calisti");
+    }
+
+    public ProductValidator(){
+        System.out.println("Yapici blok calisti");
+    }
+    public static boolean isValid(Product product){
+        if (product.price>0 && !product.name.isEmpty()){
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+    public void bisey(){
+
+    }
+        /*inner class (asagidaki classlar)
+            public static class BaskaBirClass{
+                public static void Sil(){
+
+                }
+            }*/
+}
